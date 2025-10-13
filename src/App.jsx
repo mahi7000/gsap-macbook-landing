1124243049a13d793ea5e0c42073c5cdce93f9e1
mahi7000 <mahlet7000@gmail.com>
2025-10-13 18:15:01 +0300

    Add 3D product viewer section with 14 and 16 inches in gray and black

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
         <NavBar />
         <Hero />
+        <ProductViewer />
     </main>
   )
 }
